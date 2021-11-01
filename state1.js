a2d76d32ec88d33d6cdcc47b7f89506c2a30a9fa
ajwong0105 <89656321+ajwong0105@users.noreply.github.com>
2021-11-01 12:54:05 -0500

    added text

--- state1.js
+++ state1.js
@@ -20,11 +20,22 @@ demo.state1.prototype = {
         back_wall = game.add.sprite(0, 0, "back_wall"); // NEW CAVE BACKGROUND
         back_wall.height = 400;
         back_wall.width = 600;
-        var style = { font: "bold 32px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
-        text = game.add.text(0, 200, "phaser 2.4 text bounds", style);
-        text.setTextBounds(0, 100, 600, 100);
+        var style = { font: "32px Jazz LET, fantasy", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
+        var style1 = { font: "20px Jazz LET, fantasy", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
+        text = game.add.text(0,0, "Pratorium 3000", style);
+        //text.style.fontFamily = 'Arial, Helvetica, sans-serif';
+        text.setTextBounds(0, 100, 600, -100);
+        wasd = game.add.text(0,0,"Use W, A, and D to run and jump. Hold W and either A or D to climb walls", style1);
+        wasd.setTextBounds(0, 100, 600, 0);
+        space = game.add.text(0,0, "Press space bar to shoot", style)
+        space.setTextBounds(0, 100, 600, 100,);
+
+    
     },
     update: function(){
-
+        if (game.input.activePointer.isDown){
+            console.log("click")
+            //game.state.start('state0');
+        }
     }
 };
